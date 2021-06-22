d81c628873926a128fab898d2cd068254e13c4c4
diegzumillo <diego.floor@gmail.com>
2021-06-22 20:34:41 -0300

    Trying another thing

--- res/Levels/collision_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="collision_tileset" tilewidth="8" tileheight="8" tilecount="4" columns="4">
- <image source="collision_tileset.png" trans="000000" width="32" height="8"/>
-</tileset>
--- res/Levels/simpletileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="simpletileset" tilewidth="8" tileheight="8" tilecount="25" columns="5">
- <image source="simpletileset.png" trans="000000" width="40" height="40"/>
-</tileset>
