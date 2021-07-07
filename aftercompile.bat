2a0d9a28730d19ac24796fed98e459cae8a493b6
diegzumillo <diego.floor@gmail.com>
2021-07-07 17:25:45 -0300

    push after updating gitignore

--- aftercompile.bat
+++
@@ -1,4 +0,0 @@
-%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen 
-
-
-C:\games\emulators\Fusion364-285\Fusion364\Fusion.exe %cd%/out/rom.bin
--- play.bat
+++
@@ -1,2 +0,0 @@
-
-C:\games\emulators\Fusion364-285\Fusion364\Fusion.exe %cd%/out/rom.bin
